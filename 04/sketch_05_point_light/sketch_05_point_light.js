f948e84f7bb8bf88cec857522ec82e31c641baae
Mathias Lux <mathias@juggle.at>
2023-03-30 08:12:56 +0200

    updated light example

--- 04/sketch_05_point_light/sketch_05_point_light.js
+++ 04/sketch_05_point_light/sketch_05_point_light.js
@@ -4,9 +4,9 @@ function setup() {
 
 function draw() {
   background(64);
-  // ambientLight(255, 255, 255);
-  // directionalLight(255, 255, 255, 0, -1, 0);
-  pointLight(255, 255, 255, mouseX - width/2, mouseY - height/2, 200);
+  ambientLight(64, 64, 2) 
+  // directionalLight(255, 255, 255, 0, -1, -5);
+  pointLight(255, 128, 255, mouseX - width/2, mouseY - height/2, 200);
   noStroke()
   sphere(100);
   translate(150, 0, 0);
