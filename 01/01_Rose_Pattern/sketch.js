173106c0287175ee561860644a9a53869479d7a0
Mathias Lux <mathias.lux@aau.at>
2026-02-20 11:29:32 +0100

    Integration of p5js v2.2.0 in a sample exercise.

--- 01/01_Rose_Pattern/sketch.js
+++ 01/01_Rose_Pattern/sketch.js
@@ -1,27 +1,32 @@
 // from https://en.wikipedia.org/wiki/Rose_%28mathematics%29
 let k = 4;
+let font;
 
-function setup() {
+async function setup() {
   createCanvas(400, 400);
   angleMode(DEGREES);
   stroke(255);
+  // load from Google Fonts (p5js v2.2.0):
+  font = await loadFont('https://fonts.googleapis.com/css2?family=Betania+Patmos+In&family=Edu+SA+Hand:wght@400..700&family=Noto+Color+Emoji&display=swap');
+  textFont(font)
 }
 
 function draw() {
   background(0);
   noFill();
   beginShape();
-  translate(width/2, height/2);
-  for (let i=0; i<360; i++) {
-    let r = 150*sin(k*i);
-    let x = r * sin(i)
+  translate(width / 2, height / 2);
+  for (let i = 0; i < 360; i++) {
+    let r = 150 * sin(k * i);
+    let x = r * sin(i);
     let y = r * cos(i);
-    vertex (x,y);
+    vertex(x, y);
   }
   endShape(CLOSE);
+  text("+/- for more/less petals.", -190, 190)
 }
 
 function keyReleased() {
-  if (key==='+') k++;
-  if (key==='-') k--;
+  if (key === "+") k++;
+  if (key === "-") k--;
 }
